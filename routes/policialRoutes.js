81e5857885d1b400b6f7b537b3ad5c06b3dbbdec
Pedro <tatiane.salomao2011@gmail.com>
2026-06-14 20:27:16 -0300

    atualizando projeto

--- routes/policialRoutes.js
+++ routes/policialRoutes.js
@@ -4,8 +4,6 @@ const router = express.Router();
 const ctrl = require("../controllers/policialController");
 const auth = require("../middleware/authMiddleware");
 
-router.use(auth);
-
 router.get("/", ctrl.list);
 router.post("/", ctrl.create);
 router.delete("/:id", ctrl.remove);
